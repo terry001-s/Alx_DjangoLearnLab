ff85ed4ea47aa46d6533159bc9da95eed1fa0fd9
Terry Yeboah <terryyeboah71@gmail.com>
2025-11-10 00:08:39 +0100

    Added Library import to views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
+
 # --- Function-based view for listing all books ---
 def list_books(request):
     books = Book.objects.all()
