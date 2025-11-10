22c8caacb139a6385c778027f5c50108c996b052
Terry Yeboah <terryyeboah71@gmail.com>
2025-11-10 19:19:25 +0100

    checker problem again

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
 
     # --- Secured Book Operations (permissions) ---
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+
 ]
