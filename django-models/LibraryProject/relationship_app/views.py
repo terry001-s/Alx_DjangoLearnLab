ff755cfbc5222a14876fffc7d3ac052e91f9b04a
Terry Yeboah <terryyeboah71@gmail.com>
2025-11-10 17:16:25 +0100

    Problem with checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book
+
 
 # Function-based view to list all books
 def list_books(request):
