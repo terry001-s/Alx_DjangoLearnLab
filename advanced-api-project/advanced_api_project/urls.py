654d076a2e9d5d8893df4a22f5e33f2d7d662f86
Terry Yeboah <terryyeboah71@gmail.com>
2025-11-30 16:40:51 +0100

    advanced_project_directory/urls.py fix

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Include API app URLs - this is what the checker is looking for
+    path('api/', include('api.urls')),  # Include api.urls
 ]
